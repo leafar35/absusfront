7bba65d5c4ecfbc982e28b1fed8d1f4963129f83
rafael <rafael_vianini@yahoo.com.br>
2024-01-22 09:21:37 -0300

    chore: Configuração do ambiente de dev e prod consulta api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}`
+    baseURL: (process.env.Environment === 'Production') ? `${process.env.REACT_APP_API_URL}` : `${process.env.REACT_APP_API_URL_HOMOLOGACAO}`
 })
 
 export default Api;
