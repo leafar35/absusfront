9fd1236023da35a9a506588a9191bd716dbdf5d5
rafael <rafael_vianini@yahoo.com.br>
2024-06-22 14:26:35 -0300

    feat: Adicionado calculo em horas de quanto falta para consulta.

--- src/shared/constants/key.grid.schedule.ts
+++ src/shared/constants/key.grid.schedule.ts
@@ -1,6 +1,16 @@
 import { TableColumn } from "react-data-table-component"
 import { ISchedule } from "../ischedule"
 
+const calculateTimeReal = (row: ISchedule) => {
+    let split = row?.dateTime?.toString().split(' ')
+    let format = split[0].split('/').reverse().join('-')
+    const dataNow = new Date(Date.now())
+    const dataEnd = new Date(format)
+    const diffEmMilissegundos = dataEnd.getTime() - dataNow.getTime();
+    const diffEmHoras = diffEmMilissegundos / (1000 * 60 * 60);
+    return `Falta.: ${diffEmHoras.toFixed(0)} Horas/minutos`
+}
+
 export const keysGridSchedule: TableColumn<ISchedule>[] = [
     {
         name: 'Paciente',
@@ -12,6 +22,9 @@ export const keysGridSchedule: TableColumn<ISchedule>[] = [
     },
     {
         name: 'Data da consulta',
-        selector: row => row?.dateTime?.toString(),
+        selector: (row) => {
+            const missing = calculateTimeReal(row)
+            return `${row?.dateTime?.toString()} - ${missing}`
+        },
     },
 ]
